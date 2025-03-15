77d3b465d534c702e1eae389957bc86f91a79e26
Oprea Stefan Antoniu <sopreaa@gmail.com>
2025-03-15 22:57:37 +0200

    labs/lab-03: Fix rand_r usage in shuffle function
    The code did not compile because rand_r() was called without a seed.
    
    Signed-off-by: Oprea Stefan Antoniu <sopreaa@gmail.com>

--- labs/lab-03/guides/goto-warm-up/support/bogosort/bogosort.c
+++ labs/lab-03/guides/goto-warm-up/support/bogosort/bogosort.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* checks if the array is sorted */
 static int is_sorted(int a[], int n)
@@ -20,10 +21,11 @@ static void shuffle(int a[], int n)
 {
 	int i;
 	int t, r;
+	unsigned int seed = time(NULL);
 
 	for (i = 0; i < n; i++) {
 		t = a[i];
-		r = rand_r() % n;
+		r = rand_r(&seed) % n;
 		a[i] = a[r];
 		a[r] = t;
 	}
@@ -31,18 +33,19 @@ static void shuffle(int a[], int n)
 
 int main(void)
 {
-	int numbers[] = {1, 13, 2,  5, 3, -7};
+	int numbers[] = {1, 13, 2,  5};
+	int len = sizeof(numbers) / sizeof(*numbers);
 	int i;
 
 	while (1) {
-		shuffle(numbers, 6);
+		shuffle(numbers, len);
 
-		if (is_sorted(numbers, 6))
+		if (is_sorted(numbers, len))
 			/* TODO: Use goto instead of break */
 			break;
 	}
 
-	for (i = 0; i < 6; i++)
+	for (i = 0; i < len; i++)
 		printf("%d ", numbers[i]);
 	printf("\n");
 
