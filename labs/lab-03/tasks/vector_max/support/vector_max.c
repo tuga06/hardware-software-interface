bdf1355c302083a634214f902253c23bf3b2e60f
root <root@Home-Acer-Nitro5.>
2025-03-24 16:48:09 +0200

    Update with lab 1, 2, 3

--- labs/lab-03/tasks/vector_max/support/vector_max.c
+++ labs/lab-03/tasks/vector_max/support/vector_max.c
@@ -6,11 +6,29 @@
 
 int vector_max(int *v, int len)
 {
-	int max;
-	unsigned int i;
+	if(len < 1) {
+		goto error;
+	}
+	int max = v[0];
+	unsigned int i = 0;
+	goto urmElem;
 
-	/**
-	 * TODO: Implement finding the maximum value in the vector
-	 */
+verfMax:
+	if(v[i] > max) {
+		goto actualizareMax;
+	}
+	goto urmElem;
+urmElem:
+	++i;
+	if(i >= len) {
+		goto afisare;
+	}
+	goto verfMax;
+actualizareMax:
+	max = v[i];
+	goto urmElem;
+afisare:
+	return max;
+error:
 	return -1;
 }
