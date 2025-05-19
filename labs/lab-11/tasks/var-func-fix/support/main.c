5358bac9145e84ab4fbdec9e90dea5e97d312b9b
Tudor Gabriel Cismasiu <gabrieltudor2006@gmail.com>
2025-05-19 19:23:45 +0300

    task 8

--- labs/lab-11/tasks/var-func-fix/support/main.c
+++ labs/lab-11/tasks/var-func-fix/support/main.c
@@ -7,9 +7,10 @@ int main(void)
 {
 	size_t i;
 
-	init_shopping();
-	for (i = 0; i < sizeof(shopping_list) / sizeof(shopping_list[0]); i++)
-		printf("%s\n", shopping_list[i]);
+	shopping_list();
+	//printf("%d\n", sizeof(shopping_list) / sizeof(shopping_list[0]));
+	for (i = 0; i < sizeof(init_shopping) / sizeof(init_shopping[0]); i++)
+		printf("%s\n", init_shopping[i]);
 
 	return 0;
 }
